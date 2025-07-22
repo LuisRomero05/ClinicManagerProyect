5d316d7f59de0d6fcbbee11320818654835d466b
Jisela Gutierrez <romeroluisangel735@gmail.com>
2025-07-21 22:57:30 -0600

    ACTUALÑIZAR

---
+++ ClinicManagerAPI/ClinicManagerAPI/Models/Entities/Doctor.cs
@@ -0,0 +1,21 @@
+﻿namespace ClinicManagerAPI.Models.Entities
+{
+    public class Doctor
+    {
+        public int Id { get; set; }
+        public string License { get; set; } = string.Empty;
+        public string Code { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty; // Faltaba esta propiedad
+        public string? Phone { get; set; }
+        public int SpecialtyId { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+
+        // Propiedades de navegación
+        public Specialty? Specialty { get; set; }
+        public ICollection<Appointment>? Appointments { get; set; }
+    }
+}
---
+++ ClinicManagerAPI/ClinicManagerAPI/Models/Entities/Specialty.cs
@@ -0,0 +1,17 @@
+﻿namespace ClinicManagerAPI.Models.Entities
+{
+    public class Specialty
+    {
+        public int Id { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool IsActive { get; set; } = true;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+
+        // Propiedad de navegación
+        public ICollection<Doctor> Doctors { get; set; }
+        public ICollection<Appointment> Appointments { get; set; }
+    }
+}
